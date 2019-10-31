f18f05e4e4c00531a547ee964509889b7614696a
Luís Garcés <luisgarcesleon@gmail.com>
2019-10-31 17:47:53 -0300

    firts commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
+const routes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'about/:id',
+  component: AboutComponent
+}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
